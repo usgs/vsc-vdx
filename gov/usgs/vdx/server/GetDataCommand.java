b4a09c6ec2f73d795db79d64e20262969ccf55b2
Dan Cervelli <dcervelli@users.noreply.github.com>
2005-08-28 19:15:57 +0000

    Fixes for new CodeTimer.

--- gov/usgs/vdx/server/GetDataCommand.java
+++ gov/usgs/vdx/server/GetDataCommand.java
@@ -12,6 +12,9 @@ import java.util.logging.Level;
 /**
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/08/26 20:39:00  dcervelli
+ * Initial avosouth commit.
+ *
  * @author Dan Cervelli
  */
 public class GetDataCommand extends BaseCommand
@@ -44,7 +47,7 @@ public class GetDataCommand extends BaseCommand
 			result.writeHeader(netTools, channel);
 			result.writeBody(netTools, channel);
 			ct.stop(false);
-			handler.log(Level.FINE, String.format("[getdata] %s (%d ms)", inParams.get("source"), ct.getRunTime()), channel);
+			handler.log(Level.FINE, String.format("[getdata] %s (%1.2f ms)", inParams.get("source"), ct.getRunTimeMillis()), channel);
 		}
 		else
 		{
