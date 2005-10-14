f95f896761ecb6333474bfb855d8425846e4a50e
dcervelli <dcervelli@b23e0666-5c4d-0410-a39a-a125d005d78a>
2005-10-14 21:06:26 +0000

    Now uses SQLElectronicTiltSource.

--- VDX/trunk/gov/usgs/vdx/data/tilt/ptx/ImportPTX.java
+++ VDX/trunk/gov/usgs/vdx/data/tilt/ptx/ImportPTX.java
@@ -1,20 +1,20 @@
 package gov.usgs.vdx.data.tilt.ptx;
 
-import gov.usgs.util.Util;
-import gov.usgs.vdx.data.tilt.SQLTiltDataSource;
+import gov.usgs.vdx.data.tilt.SQLElectronicTiltDataSource;
 import gov.usgs.vdx.db.VDXDatabase;
 
 import java.io.DataInputStream;
 import java.io.FileInputStream;
-import java.text.SimpleDateFormat;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.TimeZone;
 
 /**
  * Program to import Pinnacle series 5000 tiltmeter PTX files.
  *  
  * $Log: not supported by cvs2svn $
+ * Revision 1.2  2005/09/21 20:02:08  dcervelli
+ * Changed argument order.
+ *
  * 
  * @author Dan Cervelli
  */
@@ -23,44 +23,43 @@ public class ImportPTX
 
 	public static void main(String[] args) throws Exception
 	{
-		DataInputStream dis = new DataInputStream(new FileInputStream(args[0]));
-		byte[] buffer = new byte[1024];
-		boolean done = false;
-		long lastTime = 0;
-		SQLTiltDataSource dataSource = new SQLTiltDataSource();
+		SQLElectronicTiltDataSource dataSource = new SQLElectronicTiltDataSource();
 		VDXDatabase database = VDXDatabase.getVDXDatabase("vdx.config");
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("VDXDatabase", database);
-		params.put("name", args[1]);
+		params.put("name", args[0]);
+		String channel = args[1];
 		dataSource.initialize(params);
-		while (!done)
+		
+		byte[] buffer = new byte[1024];
+		boolean done = false;
+//		long lastTime = 0;
+		for (int j = 2; j < args.length; j++)
 		{
-			
-			try
+			System.out.println("file: " + args[j]);
+			DataInputStream dis = new DataInputStream(new FileInputStream(args[j]));
+			while (!done)
 			{
-				int read = 0;
-				while (read < 1024)
-					read += dis.read(buffer, read, 1024 - read);
-				
-				PTXRecord ptx = new PTXRecord(buffer);
-				if (Math.abs(lastTime - ptx.getStartTime()) > 163)
+							
+				try
 				{
-					SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-					df.setTimeZone(TimeZone.getTimeZone("GMT-8:00"));
-					System.out.println(df.format(Util.j2KToDate(Util.ewToJ2K(ptx.getStartTime()))) + " " +Math.abs(lastTime - ptx.getStartTime()));
+					int read = 0;
+					while (read < 1024)
+						read += dis.read(buffer, read, 1024 - read);
+					
+					PTXRecord ptx = new PTXRecord(buffer);
+					double[][] data = ptx.getImportData();
+					for (int i = 0; i < data.length; i++)
+					{
+						dataSource.insertData(channel, data[i][0], data[i][1], data[i][2], data[i][3], data[i][4], 0, 1, 1, 0, 0, 1, 0, 1, 0);
+					}
 				}
-				lastTime = ptx.getStartTime();
-				
-				double[][] data = ptx.getImportData();
-				for (int i = 0; i < data.length; i++)
+				catch (Exception e)
 				{
-					dataSource.insertData(args[2], data[i][0], data[i][1], data[i][2], 0, 1, 1, 0, 0);
+					done = true;
 				}
 			}
-			catch (Exception e)
-			{
-				done = true;
-			}
+			dis.close();
 		}
 	}
 }
