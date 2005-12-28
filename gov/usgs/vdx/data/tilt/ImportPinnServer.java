4481e28eafe043596fcbcad5d050dcd5b1cc055a
Tom Parker <tparker-usgs@users.noreply.github.com>
2005-12-28 20:36:59 +0000

    Add import from file, and change syntax for specifying the config file at runtime

--- gov/usgs/vdx/data/tilt/ImportPinnServer.java
+++ gov/usgs/vdx/data/tilt/ImportPinnServer.java
@@ -2,18 +2,28 @@ package gov.usgs.vdx.data.tilt;
 
 import gov.usgs.pinnacle.Client;
 import gov.usgs.pinnacle.StatusBlock;
+import gov.usgs.util.Arguments;
 import gov.usgs.util.ConfigFile;
 import gov.usgs.util.Log;
+import gov.usgs.util.ResourceReader;
 import gov.usgs.util.Util;
 import gov.usgs.vdx.db.VDXDatabase;
 
+import java.text.SimpleDateFormat;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TimeZone;
 import java.util.logging.Logger;
 
 /**
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.4  2005/10/20 05:06:06  dcervelli
+ * Fixed default xMult, yMult values.
+ *
  * Revision 1.3  2005/10/14 20:44:29  dcervelli
  * Added calibration.
  *
@@ -38,6 +48,7 @@ public class ImportPinnServer extends Client
 	private double xOffset = 0;
 	private double yOffset = 0;
 	
+		
 	public ImportPinnServer(String h, int p)
 	{
 		super(h, p);
@@ -93,12 +104,58 @@ public class ImportPinnServer extends Client
 		dataSource.insertData(channel, sb.getJ2K(), sb.getXMillis(), sb.getYMillis(), sb.getVoltage(), sb.getTemperature(), azimuth, xMult, yMult, xOffset, yOffset, 1, 0, 1, 0);
 	}
 	
-	public static void main(String[] args)
+	public void importFile(String fn)
+	{
+		logger = Log.getLogger("gov.usgs.vdx");
+		
+		try
+		{
+			ResourceReader rr = ResourceReader.getResourceReader(fn);
+			if (rr == null)
+				return;
+			logger.info("importing: " + fn);
+
+			String s = rr.nextLine();
+			while (s != null)
+			{
+				if (s.substring(21,24).equals("SB:"))
+				{
+					StatusBlock sb = new StatusBlock(Util.hexToBytes(s.substring(25)));
+					handleStatusBlock(sb);
+				}
+				s = rr.nextLine();
+			}		
+		} 
+		catch (Exception e)
+		{
+			e.printStackTrace();	
+		}
+	}
+	
+	public static void main(String[] as)
 	{
 		String cf = null;
-		if (args.length > 0)
-			cf = args[0];
+		Set<String> flags;
+		Set<String> keys;
+
+		flags = new HashSet<String>();
+		keys = new HashSet<String>();
+		keys.add("-c");
+
+		Arguments args = new Arguments(as, flags, keys);
+		
+		if (args.contains("-c"))
+			cf = args.get("-c");
+		
+		List<String> unusedArgs = args.unused();
 		ImportPinnServer ips = ImportPinnServer.createImportPinnServer(cf);
-		ips.startListening();
+		
+		if (unusedArgs.size() > 0) 
+		{
+			for (String file : unusedArgs)
+				ips.importFile(file);			
+		}
+		else
+			ips.startListening();
 	}
 }
