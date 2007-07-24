b187df62d109c4e9ab24aa2b4a6f23318c147e19
tparker <tparker@b23e0666-5c4d-0410-a39a-a125d005d78a>
2007-07-24 22:59:05 +0000

    add acitve attribute

--- VDX/trunk/gov/usgs/vdx/data/generic/GenericColumn.java
+++ VDX/trunk/gov/usgs/vdx/data/generic/GenericColumn.java
@@ -3,6 +3,9 @@ package gov.usgs.vdx.data.generic;
 /**
  * 
  * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/10/20 05:07:30  dcervelli
+ * Initial commit.
+ *
  * @author Dan Cervelli
  */
 public class GenericColumn
@@ -12,9 +15,30 @@ public class GenericColumn
 	public String description;
 	public String unit;
 	public boolean checked;
+	public boolean active;
 	
 	public GenericColumn()
 	{}
+	
+	public GenericColumn(int i, String n, String d, String u, boolean b, boolean a)
+	{
+		index = i;
+		name = n;
+		description = d;
+		unit = u;
+		checked = b;
+		active = a;
+	}
+	
+	public GenericColumn(int i, String n, String d, String u, boolean b)
+	{
+		index = i;
+		name = n;
+		description = d;
+		unit = u;
+		checked = b;
+		active = true;
+	}
 	
 	public GenericColumn(String s)
 	{
