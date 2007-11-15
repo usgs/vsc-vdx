c8e44e9c17834c29087dd452653adea87a1a16f8
uid894 <uid894@b23e0666-5c4d-0410-a39a-a125d005d78a>
2007-11-15 20:48:54 +0000

    *** empty log message ***

--- VDX/trunk/gov/usgs/vdx/data/scheduler/Scheduler.java
+++ VDX/trunk/gov/usgs/vdx/data/scheduler/Scheduler.java
@@ -3,7 +3,7 @@ package gov.usgs.vdx.data.scheduler;
 import gov.usgs.util.ConfigFile;
 import gov.usgs.util.Log;
 import gov.usgs.util.Util;
-
+import gov.usgs.vdx.data.scheduler.CopyFile;
 import java.io.*;
 import java.util.*;
 import java.util.List;
@@ -15,12 +15,14 @@ import java.util.logging.Logger;
 * @author Loren Antolik
 */
 
+// Scheduler is the main entry point for data acquisition importers.
+// it provides file management for data files, and an interface to 
+// the right importer for the job
 public class Scheduler {
 	
 	// config file variables
 	private static String		importClassName;
-	//private static List<String>	importParameters;
-	private static String		importParameters;
+	private static List<String>	importParameters;
 	private static String		sourceDirectoryName;
 	private static String		sourceFileType;
 	private static int			pollingCycleSeconds;
@@ -37,25 +39,61 @@ public class Scheduler {
 	protected Logger			logger;
 	private static File			vdxDirectory;
 	
-	// constructor
+	// constructor for no arguments (for use by a calling class)
 	public Scheduler() {
+	}
+	
+	// constructor with command line arguments (for use from the command line)
+	public Scheduler(String[] args) {
+		init(args);
+	}
+	
+	// initialization method.  initializes all values and runs the program
+	public void init(String[] args) {
+		
+		System.out.println("Scheduler:init");
+		
+		// get the config file name and make sure that it exists
+		configFileName	= args[0];
+		configFile		= new ConfigFile(configFileName);
+		if (!configFile.wasSuccessfullyRead()) {
+			System.err.printf("%s was not successfully read.\n", configFileName);
+			System.exit(-1);
+		}
+		
+		// parse out the values from the config file
+		parseConfigFileVals(configFile);
+		
+		// validate the variables that were gathered from the config file
+		validateSchedulerVars();
+		
+		// define the logger
 		logger				= Log.getLogger("gov.usgs.vdx");
 		vdxDirectory		= new File("/hvo_cluster/software/vdx");
+		
+		// setup the proper data structures based on the default vals
+		sourceDirectory				= new File(sourceDirectoryName);
+		archiveDirectory			= new File(archiveDirectoryName);
+		
+		// instantiate this scheduler class by processing the config file and it's contents
+		Scheduler scheduler	= new Scheduler();
+		Timer timer			= new Timer();
+		
+		// the config file processed okay, so go ahead and start scheduling imports
+		timer.scheduleAtFixedRate(scheduler.new SchedulerTimerTask(), 0, pollingCycleSeconds * 1000);		
 	}
 	
 	public static void parseConfigFileVals(ConfigFile configFile) {
 		
 		// read the config file and give defaults
 		importClassName				= Util.stringToString(configFile.getString("importClassName"),"");
-		// importParameters			= configFile.getList("importParameters");
-		importParameters			= Util.stringToString(configFile.getString("importParameters"), "");
+		importParameters			= configFile.getList("importParameters");
 		sourceDirectoryName			= Util.stringToString(configFile.getString("sourceDirectoryName"),"");
 		sourceFileType				= Util.stringToString(configFile.getString("sourceFileType"),"");
 		pollingCycleSeconds			= Util.stringToInt(configFile.getString("pollingCycleSeconds"), 3600);
 		archiveProcessedFile		= Util.stringToBoolean(configFile.getString("archiveProcessedFile"));
 		archiveDirectoryName		= Util.stringToString(configFile.getString("archiveDirectoryName"),"");
-		deleteProcessedFile			= Util.stringToBoolean(configFile.getString("deleteProcessedFile"));
-		
+		deleteProcessedFile			= Util.stringToBoolean(configFile.getString("deleteProcessedFile"));		
 	}
 	
 	public static void validateSchedulerVars() {
@@ -96,8 +134,7 @@ public class Scheduler {
 			} else if (!archiveDirectory.canWrite()) {
 				System.err.println("configFile:archiveDirectoryName: Directory is not writeable");
 				System.exit(-1);				
-			}
-			
+			}			
 		}
 	}
 	
@@ -122,37 +159,14 @@ public class Scheduler {
 			System.exit(-1);
 		}
 		
-		// get the config file name and make sure that it exists
-		configFileName	= args[0];
-		configFile		= new ConfigFile(configFileName);
-		if (!configFile.wasSuccessfullyRead()) {
-			System.err.printf("%s was not successfully read.\n", configFileName);
-			System.exit(-1);
-		}
-		
-		// parse out the values from the config file
-		parseConfigFileVals(configFile);
-		
-		// setup the proper data structures based on the default vals
-		sourceDirectory				= new File(sourceDirectoryName);
-		archiveDirectory			= new File(archiveDirectoryName);
-		
-		// validate the variables that were gathered from the config file
-		validateSchedulerVars();
-		
-		// instantiate this scheduler class by processing the config file and it's contents
-		Scheduler scheduler	= new Scheduler();
-		Timer timer			= new Timer();
-		
-		// the config file processed okay, so go ahead and start scheduling imports
-		timer.scheduleAtFixedRate(scheduler.new SchedulerTimerTask(), 0, pollingCycleSeconds * 1000);
+		Scheduler scheduler = new Scheduler(args);
 	}
 	
 	// extension of class for getting the proper file listing
-	class SchedulerFileFilter implements FileFilter {
+	class ImportFileFilter implements FileFilter {
 		
 		// constructor
-		public SchedulerFileFilter () {
+		public ImportFileFilter () {
 			System.out.println("SchedulerFileFilter initialized with filter " + sourceFileType);
 		}
 		
@@ -166,106 +180,66 @@ public class Scheduler {
 	class SchedulerTimerTask extends TimerTask {
 		
 		// instance variables
-		private SchedulerFileFilter		schedulerFileFilter;
+		private ImportFileFilter		importFileFilter;
 		private File[]					filesToProcess;
-		private String					fileName;
 		private File					archiveFile;
-		private Object					classObject;
-		private ProcessBuilder			processBuilder;
-		private Process					process;
-		private String					command;
-		private InputStream				is;
-		private InputStreamReader		isr;
-		private BufferedReader			br;
-		private String 					line;
-		private Map<String,String>		environment;
+		private String[]				arguments;
+		private Importer				importer;
 		
 		// constructor
 		public SchedulerTimerTask () {
-			schedulerFileFilter			= new SchedulerFileFilter();
+			importFileFilter			= new ImportFileFilter();
 		}
 		
 		// inherited method
 		public void run(){
 			
-			// check for new files
-			filesToProcess	= sourceDirectory.listFiles(schedulerFileFilter);
+			System.out.println("run:checking for new files");
 			
-			// if there are new file
+			// check for new files
+			filesToProcess	= sourceDirectory.listFiles(importFileFilter);
+			
+			// if there are new file, then we can instantiate the class, thus creating a db connection
+			// only try and make a db connection if we need one, no need having a connection open
+			// all day long if we only use it for five seconds a day.
 			if (filesToProcess.length > 0) {
 				
-				// set up the process builder
-				processBuilder	= new ProcessBuilder();
-				processBuilder.directory(vdxDirectory);
-				
-				environment			= processBuilder.environment();
-				Set keys			= environment.keySet(); 
-			    Iterator keyIter	= keys.iterator();
-			    System.out.println("The map contains the following associations:");
-			    while (keyIter.hasNext()) {
-			    	Object key = keyIter.next();
-			    	Object value = environment.get(key);
-			    	System.out.println( "   (" + key + "," + value + ")" );
-			    }
-				
-				// lets see what we are running as
-				try {
-					command = "pwd";
-					processBuilder.command(command);
-					process = processBuilder.start();
-					is		= process.getInputStream();
-					isr		= new InputStreamReader(is);
-					br		= new BufferedReader(isr);
-					System.out.println("pwd");
-					while ((line = br.readLine()) != null) {
-						System.out.println(line);
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+				System.out.println("run:new files exist");
 				
 				// instantiate the import class
-				// try {
-					// importClass	= Class.forName(importClassName);
-				// } catch (ClassNotFoundException e) {
-					// System.err.println("configFile:importClassName:ClassNotFoundException");
-					// System.exit(-1);			
-				// }
+				try {
+					importClass	= Class.forName(importClassName);
+				} catch (ClassNotFoundException e) {
+					System.err.println("configFile:importClassName:ClassNotFoundException");
+					System.exit(-1);			
+				}
 				
 				// this will call the default constructor of the class
-				// try {
-					// classObject	= importClass.newInstance();
-				// } catch (InstantiationException e) {
-					// System.err.println("configFile:importClassName:InstantiationException");
-					// System.exit(-1);			
-				// } catch (IllegalAccessException e) {
-					// System.err.println("configFile:importClassName:IllegalAccessException");
-					// System.exit(-1);			
-				// }
+				try {
+					importer	= (Importer)importClass.newInstance();
+				} catch (InstantiationException e) {
+					System.err.println("configFile:importClassName:InstantiationException");
+					System.exit(-1);			
+				} catch (IllegalAccessException e) {
+					System.err.println("configFile:importClassName:IllegalAccessException");
+					System.exit(-1);			
+				}
 				
 				// for each of the files we are processing
 				for (File file : filesToProcess) {
 					
-					// define the command for the process builder					
-					try {
-						command		= "/usr/java/default/bin/java " +
-						              "-cp lib/usgs.jar:contrib/mysql.jar:contrib/colt.jar " +
-						              importClassName + " " + 
-						              importParameters + " " +
-						              file.getAbsolutePath();
-						processBuilder.command(command);
-						process		= processBuilder.start();	
-						process.waitFor();
-					} catch (IOException e) {
-						e.printStackTrace();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-					// importClass.process(file)
+					System.out.println("run:working on " + file.getAbsolutePath());
+					
+					// add the file name to the import parameters list
+					importParameters.add(file.getAbsolutePath());
+					arguments = (String[])importParameters.toArray();
+					
+					System.out.println("run:calling init");
+					importer.init(arguments);
 			
 					// archive the file if requested
 					if (archiveProcessedFile) {
-						System.out.println("archiving " + file.getAbsolutePath());
+						System.out.println("run:archiving " + file.getAbsolutePath());
 						archiveFile	= new File(archiveDirectory, file.getName());
 						CopyFile copyFile = new CopyFile();
 						try {
@@ -277,16 +251,12 @@ public class Scheduler {
 			
 					// rename the file if requested
 					if (deleteProcessedFile) {
-						System.out.println("deleting " + file.getAbsolutePath());
+						System.out.println("run:deleting " + file.getAbsolutePath());
 						if (!file.delete()) {
 							System.err.println("error deleting source file " + file.getName());
 						}
 					}
-				}
-			
-				// close the class and the db connection
-				// class.close();
-			
+				}			
 			}
 		}
 	}
