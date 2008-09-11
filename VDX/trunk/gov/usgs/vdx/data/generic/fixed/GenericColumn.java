06acd0afbfb06686e6be0a58df0ba0474e09a786
tparker <tparker@b23e0666-5c4d-0410-a39a-a125d005d78a>
2008-09-11 20:43:21 +0000

    Refactor Generic and NWIS naming

---
+++ VDX/trunk/gov/usgs/vdx/data/generic/fixed/GenericColumn.java
@@ -0,0 +1,57 @@
+package gov.usgs.vdx.data.generic.fixed;
+
+/**
+ * 
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2005/10/20 05:07:30  dcervelli
+ * Initial commit.
+ *
+ * @author Dan Cervelli
+ */
+public class GenericColumn
+{
+	public int index;
+	public String name;
+	public String description;
+	public String unit;
+	public boolean checked;
+	public boolean active;
+	
+	public GenericColumn()
+	{}
+	
+	public GenericColumn(int i, String n, String d, String u, boolean b, boolean a)
+	{
+		index = i;
+		name = n;
+		description = d;
+		unit = u;
+		checked = b;
+		active = a;
+	}
+	
+	public GenericColumn(int i, String n, String d, String u, boolean b)
+	{
+		index = i;
+		name = n;
+		description = d;
+		unit = u;
+		checked = b;
+		active = true;
+	}
+	
+	public GenericColumn(String s)
+	{
+		String[] ss = s.split(":");
+		index = Integer.parseInt(ss[0]);
+		name = ss[1];
+		description = ss[2];
+		unit = ss[3];
+		checked = ss[4].equals("T");
+	}
+	
+	public String toString()
+	{
+		return(String.format("%d:%s:%s:%s:%s", index, name, description, unit, (checked ? "T" : "F")));
+	}
+}
---
+++ VDX/trunk/gov/usgs/vdx/data/generic/variable/NWISData.java
@@ -0,0 +1,34 @@
+package gov.usgs.vdx.data.generic.variable;
+
+import gov.usgs.vdx.data.GenericDataMatrix;
+
+import java.util.List;
+
+/**
+ * 
+ * $Log: not supported by cvs2svn $
+ * Revision 1.1  2006/08/01 19:54:47  tparker
+ * Create NWIS data source
+ *
+ *
+ * @author Tom Parker
+ */
+public class NWISData extends GenericDataMatrix
+{
+	public NWISData()
+	{
+		super();
+	}
+	
+	public NWISData(List<double[]> pts)
+	{
+		super(pts);
+	}
+	
+	public void setColumnNames()
+	{
+		columnMap.put("time", 0);
+		columnMap.put("type", 1);
+		columnMap.put("value", 2);
+	}
+}
