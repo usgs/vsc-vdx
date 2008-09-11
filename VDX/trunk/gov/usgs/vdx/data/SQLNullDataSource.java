bb34e01446267e7f41f5cc169acad6850c3a3b19
tparker <tparker@b23e0666-5c4d-0410-a39a-a125d005d78a>
2008-09-11 20:57:56 +0000

    Refactor Generic and NWIS naming

--- VDX/trunk/gov/usgs/vdx/data/SQLNullDataSource.java
+++ VDX/trunk/gov/usgs/vdx/data/SQLNullDataSource.java
@@ -2,37 +2,15 @@ package gov.usgs.vdx.data;
 
 import gov.usgs.util.ConfigFile;
 import gov.usgs.util.Util;
-import gov.usgs.vdx.data.DataSource;
-import gov.usgs.vdx.data.SQLDataSource;
-import gov.usgs.vdx.data.nwis.DataType;
-import gov.usgs.vdx.data.nwis.Station;
-import gov.usgs.vdx.db.VDXDatabase;
-import gov.usgs.vdx.server.BinaryResult;
 import gov.usgs.vdx.server.RequestResult;
-import gov.usgs.vdx.server.TextResult;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
 
 import cern.colt.matrix.DoubleMatrix2D;
 
 /**
  * 
- * $Log: not supported by cvs2svn $
- * Revision 1.3  2007/04/30 05:28:12  tparker
- * initial commit for rsam SQL Bob importer
- *
- * Revision 1.2  2007/04/25 08:03:16  tparker
- * cleanup
- *
- * Revision 1.1  2007/04/22 06:42:26  tparker
- * Initial ewrsam commit
  *
  * @author Tom Parker
  */
