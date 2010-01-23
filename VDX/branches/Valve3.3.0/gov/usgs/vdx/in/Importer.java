018dec3913b2300cfef13a01954e1eff3b4d3f3a
loren <loren@b23e0666-5c4d-0410-a39a-a125d005d78a>
2010-01-23 06:20:57 +0000

    Initial Commit

--- VDX/branches/Valve3.3.0/gov/usgs/vdx/in/Importer.java
+++ VDX/branches/Valve3.3.0/gov/usgs/vdx/in/Importer.java
@@ -1,13 +1,21 @@
 package gov.usgs.vdx.in;
 
 import gov.usgs.util.ConfigFile;
+import gov.usgs.util.ResourceReader;
+import gov.usgs.vdx.data.Channel;
+import gov.usgs.vdx.data.Column;
 import gov.usgs.vdx.data.Rank;
 import gov.usgs.vdx.data.SQLDataSource;
 import gov.usgs.vdx.data.SQLDataSourceDescriptor;
 import gov.usgs.vdx.data.SQLDataSourceHandler;
 
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.logging.Logger;
 import java.util.logging.Level;
@@ -21,6 +29,8 @@ import java.util.logging.Level;
  */
 abstract public class Importer {
 	
+	protected ResourceReader rr;
+	
 	protected static Set<String> flags;
 	protected static Set<String> keys;
 	
@@ -28,24 +38,62 @@ abstract public class Importer {
 	
 	protected ConfigFile params;
 	protected ConfigFile vdxParams;
+	protected ConfigFile rankParams;
+	protected ConfigFile columnParams;
+	protected ConfigFile channelParams;
+	protected ConfigFile dataSourceParams;
+	protected ConfigFile translationParams;
 	
 	protected String driver, prefix, url;
 	
-	protected String dataSource;
-	protected SQLDataSource sqlDataSource;
-	protected SQLDataSourceHandler sqlDataSourceHandler;
-	protected SQLDataSourceDescriptor sqlDataSourceDescriptor;
-	
 	protected SimpleDateFormat dateIn;
+	protected SimpleDateFormat dateOut;
+	protected Date date;
+	protected Double j2ksec;
 
+	protected String filenameMask;
 	protected int headerLines;
 	protected String timestampMask;
 	protected String timeZone;
 	
+	protected String importColumns;
+	protected String[] importColumnArray;
+	protected Map<Integer, String> importColumnMap;
+	
+	protected String dataSource;
+	protected SQLDataSource sqlDataSource;
+	protected SQLDataSourceHandler sqlDataSourceHandler;
+	protected SQLDataSourceDescriptor sqlDataSourceDescriptor;	
+	protected List<String> dataSourceList;
+	protected Map<String, String> dataSourceColumnMap;
+	protected Map<String, String> dataSourceChannelMap;
+	protected Map<String, SQLDataSource> sqlDataSourceMap;
+	protected Iterator<String> dsIterator;
+	
 	protected Rank rank;
 	protected String rankName;
 	protected int rankValue, rankDefault;
-	protected ConfigFile rankParams;
+	
+	protected String channels;
+	protected String[] channelArray;
+	protected Map<String, Channel> channelMap;	
+	protected Channel channel;
+	protected String channelCode, channelName;
+	protected double channelLon, channelLat, channelHeight;
+	protected List<String> channelList;
+	protected Iterator<String> chIterator;
+	protected String defaultChannels;
+	
+	protected String columns;
+	protected String[] columnArray;
+	protected HashMap<String, Column> columnMap;	
+	protected Column column;
+	protected String columnName, columnDescription, columnUnit;
+	protected int columnIdx;
+	protected boolean columnActive, columnChecked;
+	protected List<String> columnList;
+	protected Iterator<String> coIterator;
+	protected String defaultColumns;
 	
 	protected String importerType;
 	
