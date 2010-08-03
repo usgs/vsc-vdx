d40b40a062e60fc5c9f9972b99e719630e22036a
istisvn <istisvn@b23e0666-5c4d-0410-a39a-a125d005d78a>
2010-08-03 21:01:13 +0000

    Bugfixes for ticket #2

--- VDX/branches/isti_3.4/perl/exportData.pl
+++ VDX/branches/isti_3.4/perl/exportData.pl
@@ -1,24 +1,31 @@
 #!/usr/bin/perl
 use URI::Escape;
  
-# The URL w/ request parameters
+# The URL w/o request parameters
 my $csv_request_path = "http://localhost:8080/valve3/valve3.jsp";
 
-# Build the arguments string
+# Check the number of arguments
 my $argc = $#ARGV + 1;
-
 die "Wrong number of arguments: " . $argc  unless $argc % 2 == 0;
 
-my $abortmsg = "";
+my $abortmsg = "";	# if not "", abort w/ this message
 my $key;
-my $vkey;
-my $querymap = {};
-my @def;
+my $querymap = {};	# map of query args to their valuse
 
+# argument definitions
+# 	1st char indicates kind of argument:
+#		! = exactly 1 instance allowed
+#		? = up to 1 instance allowed
+#		+ = 0 or more instances allowed
+#			(values will be comma-separated)
+#		* = special case for columns
+#	Rest of value is name used in query string
 my %arg_def = ("source"=>"!src.0", "channel"=>"!chNames.0", "start"=>"!st.0", 
 	"end"=>"?et.0", "timezone"=>"?tz", 
-	"datatype"=>"+dt", "column"=>"+colNames.0", "rank"=>"?rkName" );
+	"datatype"=>"+dt", "column"=>"*colNames", "rank"=>"?rkName" );
+my %cols = ();
 
+# Walk the command-line arguments, filling querymap
 for ( $i=0; $i<$argc; $i += 2 ) {
 	if ( substr( $ARGV[$i], 0, 1 ) eq "-" ) {
 		$key = substr( $ARGV[$i], 1 );
@@ -27,14 +34,23 @@ for ( $i=0; $i<$argc; $i += 2 ) {
 			$abortmsg = $abortmsg . "Unknown argument: -" . $key . "\n";
 			next;
 		}
-		$def = substr($arg_def{ $key },0,1);
+		my $def = substr($arg_def{ $key },0,1);
 		my $qkey = substr( $arg_def{ $key }, 1 );
 		if ( exists $querymap{$qkey} ) {
 			if ( $def eq "+" ) {
 				$querymap{$qkey} = $querymap{$qkey} ."," . $ARGV[$i+1];
+			} elsif ( $def eq "*" ) {
+				if ( exists $cols{$ARGV[$i+1]} ) {
+					$abortmsg = $abortmsg . "-" . $key . " " . $ARGV[$i+1] . " is repeated\n";
+				} else {
+					$cols{$ARGV[$i+1]} = "T";
+				}
 			} else {
-				$abortmsg = $abortmsg . "-" . $key . " is repeated\n";
+				$abortmsg = $abortmsg . "-" . $key . " is repeated (" . $def . $qkey . ") '" . $arg_def{ $key } . "'\n";
 			}
+		} elsif ( $def eq "*" ) {
+			$querymap{$qkey} = 1;
+			$cols{$ARGV[$i+1]} = "T";
 		} else {
 			$querymap{$qkey} = $ARGV[$i+1];
 		}
@@ -43,6 +59,7 @@ for ( $i=0; $i<$argc; $i += 2 ) {
 	}
 }
 
+# Now check for presence of required arguments
 while (($key,$spec) = each ( %arg_def )) {
 	if ( substr($key,0,4) eq "HASH" ) {
 		next;
@@ -54,6 +71,7 @@ while (($key,$spec) = each ( %arg_def )) {
 	}
 }
 
+# Validate -output value; handle default
 if ( exists $querymap{ "o" } ) {
 	my $o = $querymap{ "o" };
 	if ( $o eq "csv" ) {
@@ -67,40 +85,37 @@ if ( exists $querymap{ "o" } ) {
 	$querymap{"o"} = "csv";
 }
 
-if ( ! exists $querymap{ "tz" } ) {
-	$querymap{"tz"} = "UTC";
-}
-
-while (($key,$vkey) = each ( %querymap )) {
-	print "argmap[" . $key . "] = " . $vkey . "\n";
-}
-
 die $abortmsg unless $abortmsg eq "";
 
+# Handle default for -timezone
+#if ( ! exists $querymap{ "tz" } ) {
+#	$querymap{"tz"} = "UTC";
+#}
+
+# Handle columns
+foreach $key (keys %cols) {
+	$querymap{ $key . ".0" } = "T";
+}
+
+# Build the string of query arguments
 my $csv_request_args = "&chCnt.0=1";
+my $vkey;
 while (($key,$vkey) = each ( %querymap ) ) {
 	$csv_request_args = $csv_request_args . "&" . $key . "=" . uri_escape( $vkey );
 }
 
-# Now we can build the URL
+# Now we can build the whole URL
 my $csv_request_url = $csv_request_path . "?a=rawData" . $csv_reqtype . $csv_request_args;
 
 # Make request of server; should get back XML containing data file's URL
-#use LWP::Simple;
-#my $xml_with_csv_url = get ($csv_request_url);
-#die "Initial request failed" unless defined $xml_with_csv_url;
 use LWP;
 my $browser = LWP::UserAgent->new;
-
-print $csv_request_url . "\n";
 
 my $response = $browser->get( $csv_request_url );
 die "Can't get $url -- ", $response->status_line
 	unless $response->is_success;
-
-die "Hey, I was expecting XML, not ", $response->content_type
+die "Was expecting XML; got ", $response->content_type
 	unless $response->content_type eq 'text/xml';
-	# or whatever content-type you're equipped to deal with
 
 my $xml_with_csv_url = $response->content;
 
@@ -115,8 +130,9 @@ $response = $browser->get( $csv_url );
 die "Can't get $url -- ", $response->status_line
 	unless $response->is_success;
 
-die "Hey, I was expecting HTML, not ", $response->content_type
-	unless $response->content_type eq 'application/octet-stream';
+my $type_expected = 'application/octet-stream';
+die "Was expecting " . $type_expected . "; got ", $response->content_type
+	unless $response->content_type eq $type_expected;
 
 # Dump the csv file
 print $response->content;
