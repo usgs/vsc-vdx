99bb5242f8b543fa4b78c351f0ee56ed5137505a
loren <loren@b23e0666-5c4d-0410-a39a-a125d005d78a>
2010-12-02 03:32:42 +0000

    Modified column names and units to better describe and fit on the menu

--- VDX/branches/isti_3.4/gov/usgs/vdx/data/tilt/SQLTiltDataSource.java
+++ VDX/branches/isti_3.4/gov/usgs/vdx/data/tilt/SQLTiltDataSource.java
@@ -32,25 +32,25 @@ public class SQLTiltDataSource extends SQLDataSource implements DataSource {
 	public static final boolean menuColumns		= true;
 	
 	public static final Column[] DATA_COLUMNS	= new Column[] {
-		new Column(1, "xTilt",		"East",					"tilt (microradians)",	false, true, false),
-		new Column(2, "yTilt",		"North",				"tilt (microradians)",	false, true, false), 
-		new Column(3, "holeTemp",	"Hole Temperature",		"temperature (C)",		false, true, true), 
-		new Column(4, "boxTemp",	"Box Temperature",		"temperature (C)",		false, true, true),
-		new Column(5, "instVolt",	"Instrument Voltage",	"voltage (V)",			false, true, true),
-		new Column(6, "gndVolt",	"Ground Voltage",		"voltage (V)",			false, true, false),
-		new Column(7, "rainfall",	"Rainfall",				"rainfall (cm)",		false, true, true)};
-	
+		new Column(1, "xTilt",		"East",			"microrad",	false, true, false),
+		new Column(2, "yTilt",		"North",		"microrad",	false, true, false), 
+		new Column(3, "holeTemp",	"Hole Temp",	"degrees C",false, true, true), 
+		new Column(4, "boxTemp",	"Box Temp",		"degrees C",false, true, true),
+		new Column(5, "instVolt",	"Inst Voltage",	"volts",	false, true, true),
+		new Column(6, "gndVolt",	"Grnd Voltage",	"volts",	false, true, false),
+		new Column(7, "rainfall",	"Rainfall",		"cm",		false, true, true)};
+
 	public static final Column[] MENU_COLUMNS	= new Column[] {
-		new Column(1, "radial",		"Radial",				"tilt (microradians)",	true,	true,  false),
-		new Column(2, "tangential",	"Tangential",			"tilt (microradians)",	true,	true,  false), 
-		new Column(3, "xTilt",		"East",					"tilt (microradians)",	false,	true,  false),
-		new Column(4, "yTilt",		"North",				"tilt (microradians)",	false,	true,  false), 
-		new Column(5, "magnitude",	"Magnitude",			"tilt (microradians)",	false,	false, false),
-		new Column(6, "azimuth",	"Azimuth",				"tilt (microradians)",	false,	false, false), 
-		new Column(7, "holeTemp",	"Hole Temperature",		"temperature (C)",		false,	false, false), 
-		new Column(8, "boxTemp",	"Box Temperature",		"temperature (C)",		false,	false, true),
-		new Column(9, "instVolt",	"Instrument Voltage",	"voltage (V)",			false,	false, true),
-		new Column(10, "rainfall",	"Rainfall",				"rainfall (cm)",		false,	false, true)};
+		new Column(1, "radial",		"Radial",		"microrad",	true,	true,  false),
+		new Column(2, "tangential",	"Tangential",	"microrad",	true,	true,  false), 
+		new Column(3, "xTilt",		"East",			"microrad",	false,	true,  false),
+		new Column(4, "yTilt",		"North",		"microrad",	false,	true,  false), 
+		new Column(5, "magnitude",	"Magnitude",	"microrad",	false,	false, false),
+		new Column(6, "azimuth",	"Azimuth",		"microrad",	false,	false, false), 
+		new Column(7, "holeTemp",	"Hole Temp",	"degrees C",false,	false, false), 
+		new Column(8, "boxTemp",	"Box Temp",		"degrees C",false,	false, true),
+		new Column(9, "instVolt",	"Inst Voltage",	"volts",	false,	false, true),
+		new Column(10, "rainfall",	"Rainfall",		"cm",		false,	false, true)};
 
 	/**
 	 * Get database type, generic in this case
