52729323a2a6373caa807f24a9a59c2978df9470
tparker <tparker@b23e0666-5c4d-0410-a39a-a125d005d78a>
2013-01-27 23:20:28 +0000

    xTick every minute on the minute. Needs to be generalized.

---
+++ VDX/trunk/gov/usgs/vdx/data/wave/plot/MinuteMarkingWaveRenderer.java
@@ -0,0 +1,62 @@
+package gov.usgs.vdx.data.wave.plot;
+
+import gov.usgs.plot.AxisRenderer;
+import gov.usgs.plot.SmartTick;
+
+import java.util.ArrayList;
+
+/**
+ * Customized SliceWaveRenderer
+ * This really needs to be generalized, but it works for now...
+ * 
+ * TODO: generalize
+ * 
+ * @author Tom Parker
+ */
+public class MinuteMarkingWaveRenderer extends SliceWaveRenderer {
+	
+	/**
+	 * Set axis ticks and labels values 
+	 */
+	public void update()
+	{
+		this.setExtents(viewStartTime, viewEndTime, minY, maxY);
+		int hTicks = graphWidth / 108;
+		int vTicks = graphHeight / 24;
+		this.createAxis(vTicks);
+		this.setXAxisToTime(hTicks, false, false);
+		this.getAxis().setInnerLeftLabelAsText(yLabelText, -46);
+		if (title != null)
+			this.getAxis().setLeftLabelAsText(title, -56);
+	}
+
+
+	/**
+	 * create axis with x-axis tick every minute 
+	 * @param vTicks
+	 */
+	public void createAxis(int vTicks)
+    {
+    	if (axis == null)
+    		axis = new AxisRenderer(this);
+        axis.createDefault();
+       	createDefaultYAxis(vTicks, yTickMarks, yTickValues, true);
+
+       	int firstTick = (int)(getMinXAxis() + (1 - (getMinXAxis() % 60)));
+       	int lastTick = (int)(getMaxXAxis() - (getMinXAxis() % 60));
+       	int tickCount = 1 + (lastTick - firstTick) / 60;
+       	double[] tickList = new double[tickCount];
+       	for (int i = 0; i < tickCount; i++)
+       		tickList[i] = firstTick + i * 60;
+       	
+        if(xTickMarks){
+        	axis.createTopTicks(tickList);
+        	axis.createBottomTicks(tickList);
+        	axis.createVerticalGridLines(tickList);
+        }
+        if(xTickValues){
+        	axis.createBottomTickLabels(tickList, null);
+        }
+     }
+
+}
