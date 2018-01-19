5b0358ff372b4a00ee1433f66c03a345b32328ab
Bill Tollett <wtollett-usgs@users.noreply.github.com>
2018-01-18 14:11:26 -1000

    Updated to work with Winston 1.3.0; Checkstyle fixes.

--- src/main/java/gov/usgs/volcanoes/vdx/data/rsam/RatSAMSource.java
+++ src/main/java/gov/usgs/volcanoes/vdx/data/rsam/RatSAMSource.java
@@ -3,37 +3,36 @@ package gov.usgs.volcanoes.vdx.data.rsam;
 import gov.usgs.math.DownsamplingType;
 import gov.usgs.plot.data.BinaryDataSet;
 import gov.usgs.plot.data.RSAMData;
+import gov.usgs.volcanoes.core.data.Scnl;
 import gov.usgs.volcanoes.core.util.UtilException;
 import gov.usgs.volcanoes.vdx.data.VDXSource;
 
 /**
- * 
- * $Log: not supported by cvs2svn $
+ * RatSAM datasource.
  *
  * @author Tom Parker
+ * @author Bill Tollett
  */
-public class RatSAMSource extends VDXSource
-{
-	public String getType()
-	{
-		return "rsam";
-	}
-	
-	public void disconnect() {
-		defaultDisconnect();
-	}
+public class RatSAMSource extends VDXSource {
 
-	protected BinaryDataSet getData(String channel, double st, double et, int maxrows, DownsamplingType ds, int dsInt) throws UtilException
-	{
-		RSAMData d1 = null;
-		RSAMData d2 = null;
-		String[] channels = channel.split(",");
-		if (channels.length == 2)
-		{
-			d1 = data.getRSAMData(channels[0], st, et, maxrows, ds, dsInt);
-			d2 = data.getRSAMData(channels[1], st, et, maxrows, ds, dsInt);
-		}
-		
-		return (BinaryDataSet) d1.getRatSAM(d2);
-	}
+  public String getType() {
+    return "rsam";
+  }
+
+  public void disconnect() {
+    defaultDisconnect();
+  }
+
+  protected BinaryDataSet getData(String channel, double st, double et, int maxrows,
+      DownsamplingType ds, int dsInt) throws UtilException {
+    RSAMData d1       = null;
+    RSAMData d2       = null;
+    String[] channels = channel.split(",");
+    if (channels.length == 2) {
+      d1 = data.getRSAMData(Scnl.parse(channels[0]), st, et, maxrows, ds, dsInt);
+      d2 = data.getRSAMData(Scnl.parse(channels[1]), st, et, maxrows, ds, dsInt);
+    }
+
+    return (BinaryDataSet) d1.getRatSAM(d2);
+  }
 }
